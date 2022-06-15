17068eedbd25d7a85c13281ebb68a6c94d2dd7d8
Ademir Ahmetovic <80320950+ademirson1g@users.noreply.github.com>
2022-06-15 22:55:24 +0200

    Update SupportBot.jsx

--- src/components/SupportBot/SupportBot.jsx
+++ src/components/SupportBot/SupportBot.jsx
@@ -5,7 +5,7 @@ export class SupportBot extends Component {
     componentDidMount() {
     (function(d, m){
         var kommunicateSettings = 
-            {"appId":"1d21cdde85157fffa1727322de3bfc3fa","popupWidget":true,"automaticChatOpenOnNavigation":true};
+            {"appId":"Your Api ID","popupWidget":true,"automaticChatOpenOnNavigation":true};
         var s = document.createElement("script"); s.type = "text/javascript"; s.async = true;
         s.src = "https://widget.kommunicate.io/v2/kommunicate.app";
         var h = document.getElementsByTagName("head")[0]; h.appendChild(s);
